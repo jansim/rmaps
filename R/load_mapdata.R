15340db89dd51920925cde2b70fa4431daba5e55
Jan Simson <jan.simson@three-2-one.com>
2020-11-17 20:54:29 +0100

    Auto-detect which data to load in `load_mapdata` + support coastline data

--- R/load_mapdata.R
+++ R/load_mapdata.R
@@ -1,4 +1,4 @@
-load_mapdata <- function (data_id, water = T, buildings = F, roads = T) {
+load_mapdata <- function (data_id, water = NULL, buildings = NULL, roads = NULL, coast = NULL) {
 
   output <- list(
     id = data_id
@@ -6,20 +6,23 @@ load_mapdata <- function (data_id, water = T, buildings = F, roads = T) {
 
   load(file = get_cache_path(data_id = data_id, type = "boundingbox"))
   output$bb = boundingbox
-
-  if (water) {
-    load(file = get_cache_path(data_id = data_id, type = "water"))
-    output$water <- water_data
-  }
-
-  if (buildings) {
-    load(file = get_cache_path(data_id = data_id, type = "buildings"))
-    output$buildings <- buildings_data
-  }
-
-  if (roads) {
-    load(file = get_cache_path(data_id = data_id, type = "roads"))
-    output$roads <- roads_data
+  
+  types <- c(
+    "water",
+    "buildings",
+    "roads",
+    "coast"
+  )
+  
+  # Load different types of spatial data
+  for (type in types) {
+    cache_path <- get_cache_path(data_id = data_id, type = type)
+    # If not explicitly provided, default to load all data if its cache file exists
+    do_load <- if (is.null(get(type))) { file.exists(cache_path) } else { get(type) }
+    if (do_load) {
+      load(file = cache_path)
+      output[[type]] <- get(paste0(type, "_data")) 
+    } 
   }
 
   return(output)
