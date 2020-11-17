cc6ce14278008cba8e47153ade9bdad2bee68044
Jan Simson <jan.simson@three-2-one.com>
2020-11-17 20:41:04 +0100

    Also download coast data

--- R/download_mapdata.R
+++ R/download_mapdata.R
@@ -1,13 +1,18 @@
-download_mapdata <- function (data_id, boundingbox, water = T, buildings = F, roads = T) {
+download_mapdata <- function (data_id, boundingbox, water = T, buildings = F, roads = T, coast = T) {
   
   # Save bounding box as well
   save(boundingbox, file = get_cache_path(data_id = data_id, type = "boundingbox"))
 
   if (water) {
-    water_data <- boundingbox %>%
+    natural_water_data <- boundingbox %>%
       opq() %>%
       add_osm_feature(key = "natural", value = "water") %>%
-      osmdata_sf() 
+      osmdata_sf()
+    riverbanks_water_data <- boundingbox %>%
+      opq() %>%
+      add_osm_feature(key = "waterway", value = "riverbank") %>%
+      osmdata_sf()
+    water_data <- c(natural_water_data, riverbanks_water_data)
     save(water_data, file = get_cache_path(data_id = data_id, type = "water"))
   }
   
@@ -25,6 +30,14 @@ download_mapdata <- function (data_id, boundingbox, water = T, buildings = F, ro
       add_osm_feature(key = "highway") %>%
       osmdata_sf()
     save(roads_data, file = get_cache_path(data_id = data_id, type = "roads"))
+  }
+  
+  if (coast) {
+    coast_data <- boundingbox %>%
+      opq() %>%
+      add_osm_feature(key = "natural", value = "coastline") %>%
+      osmdata_sf() 
+    save(coast_data, file = get_cache_path(data_id = data_id, type = "coast"))
   }
   
 }
